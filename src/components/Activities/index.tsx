9ca586c31b5ca1dcae8c47bf978c7dc973ebf161
Elias <eliasnemr@gmail.com>
2024-07-26 12:27:54 +0300

    Testing fetching ERC-20 Transfers for Account

--- src/components/Activities/index.tsx
+++ src/components/Activities/index.tsx
@@ -1,20 +1,23 @@
 import { appContext } from "../../AppContext";
 import { useContext } from "react";
+import useBlockchainHistory from "../../hooks/useBlockchainHistory";
 
 
 const Activity = () => {
   const { _currentNavigation } =
     useContext(appContext);
+  useBlockchainHistory();
 
-  if (_currentNavigation !== "activity") {
+  if (_currentNavigation !== "history") {
     return null;
   }
+
   return (
     <div className="px-4 md:px-0">
       <h3 className="font-bold">Activities</h3>
 
       <ul className="mt-2">
-        
+
       </ul>
     </div>
   );
--- src/components/DesktopNav/index.tsx
+++ src/components/DesktopNav/index.tsx
@@ -1,7 +1,5 @@
 import { useContext } from "react";
 import { appContext } from "../../AppContext";
-import { useWalletContext } from "../../providers/WalletProvider/WalletProvider";
-import { etherscan } from "../../constants";
 import NavigationButton from "../UI/NavigationButton";
 import SendIcon from "../UI/Icons/SendIcon";
 import SwapIcon from "../UI/Icons/SwapIcon";
@@ -9,21 +7,13 @@ import HistoryIcon from "../UI/Icons/HistoryIcon";
 
 const DesktopNav = () => {
   const { _currentNavigation, handleNavigation } = useContext(appContext);
-  const { _address, _network } = useWalletContext();
 
   return (
     <section>
       <nav className="grid-cols-3 gap-2 grid-rows-[58px] hidden sm:grid">        
         <NavigationButton title="Send" icon={<SendIcon />} onClick={() => handleNavigation("send")} active={_currentNavigation==='send'} />
         <NavigationButton title="Swap" icon={<SwapIcon />} onClick={() => handleNavigation("uniswap")} active={_currentNavigation==='uniswap'} />
-        <NavigationButton title="History" icon={<HistoryIcon />} onClick={(e) => {
-          if (window.navigator.userAgent.includes("Minima Browser")) {
-            e.preventDefault();
-            // @ts-ignore
-            Android.openExternalBrowser(`${etherscan[_network].rpc}${_address}`, "_blank");
-          }
-
-          window.open(`${etherscan[_network].rpc}${_address}`, "_blank")}} active={false} />
+        <NavigationButton title="History" icon={<HistoryIcon />} onClick={() => handleNavigation("history")} active={false} />
       </nav>
     </section>
   );
